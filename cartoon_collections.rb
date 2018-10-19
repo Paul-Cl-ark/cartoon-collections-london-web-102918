d743ed2ba7a05cf41fd15ff0dd3bb0e45c18a463
Paul Clark <clark.paul123@gmail.com>
2018-10-19 16:01:49 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(elements)
   elements.collect {|e| "#{e.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.find()
 end
 
 def find_the_cheese# code an argument here
