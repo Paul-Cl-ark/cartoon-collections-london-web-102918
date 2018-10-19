a5284fbf42d0a2edfa9809e7eb5d56c1da72694a
Paul Clark <clark.paul123@gmail.com>
2018-10-19 16:17:17 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,6 +11,6 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(food)
-    food.find do |i| i == "cheddar" || i == "gouda" || i == "camembert"}
+    food.find do |i| i == "cheddar" || i == "gouda" || i == "camembert"
   end
 end
